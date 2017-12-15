98e1ccc4ce34bc3240ea6e65710b721ac930737a
bank <jak.yns@gmail.com>
2017-12-15 18:44:30 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Omise Python Tools
+
+create `development.cfg` or `production.cfg` by copying templates from `default.cfg` and fill in the values
+
+## charges wave
+
+Python script for creating amount of charges as you want
+
+### Usage
+
+```python
+>>> import omise
+>>> python charges_wave.py {charges_amount}
+```
---
+++ charges_wave.py
@@ -0,0 +1,32 @@
+import configparser
+import os
+import random
+import sys
+
+import omise
+
+ENV_MODE = os.environ.get('ENV_MODE', 'development')
+
+config = configparser.ConfigParser()
+config.read_file(open('default.cfg'))
+config.read('{}.cfg'.format(ENV_MODE))
+
+omise.api_main = config['omise']['api_main']
+omise.api_vault = config['omise']['api_vault']
+omise.api_secret = config['omise']['api_secret']
+omise.api_public = config['omise']['api_public']
+
+charges_amount = int(sys.argv[1])
+
+customer = omise.Customer.retrieve('cust_test_5aa1rasgyscgk8ra7w6')
+
+for n in range(1, charges_amount):
+    charge = omise.Charge.create(
+        amount=int(str(random.randint(20, 100)) + '00'),
+        currency='thb',
+        description='charge no.' + str(random.randrange(100)),
+        customer=customer.id,
+        card='card_test_5aa1rarjac6tnqcdhmx',
+    )
+
+print('{} charges have been created'.format(charges_amount))
