63773e766d728d944c4681e951b92a5cf6896291
Jakkrij Yoonaisil <jakkrij@omise.co>
2018-02-22 14:14:06 +0700

    Cards manufacture

---
+++ cards_manufacture.py
@@ -0,0 +1,35 @@
+import configparser
+import os
+import sys
+
+import omise
+
+ENV_MODE = os.environ.get('ENV_MODE', 'development')
+
+config = configparser.ConfigParser()
+config.read_file(open('default.cfg'))
+config.read('{}.cfg'.format(ENV_MODE))
+
+omise.api_main = config['omise']['api_main']
+omise.api_vault = config['omise']['api_vault']
+omise.api_secret = config['omise']['api_secret']
+omise.api_public = config['omise']['api_public']
+
+cards_amount = int(sys.argv[1])
+
+customer = omise.Customer.retrieve('cust_test_5azhl5kaxja800zsyfs')
+
+for n in range(1, cards_amount):
+    token = omise.Token.create(
+        name='Somchai Prasert',
+        number='4242424242424242',
+        expiration_month=10,
+        expiration_year=2019,
+        city='Bangkok',
+        postal_code='10320',
+        security_code=123
+    )
+
+    customer.update(card=token.id)
+
+print('{} cards have been created for {}'.format(cards_amount, customer.id))
