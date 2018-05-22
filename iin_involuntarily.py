593b1f793c2d76058d1f9a9c78d6cb96fac8c0ab
bank <jak.yns@gmail.com>
2018-05-22 23:17:29 +0700

    Add pipenv files

--- iin_involuntarily.py
+++ iin_involuntarily.py
@@ -4,9 +4,16 @@ import random
 import time
 
 iin_list = ['439137', '520000', '400555', '356342']
+round = 0
 
 while True:
-    start = time.clock()
-    r = requests.get("https://iin.omise.co/v1/%s" % random.choice(iin_list), timeout=1)
-    request_time = time.clock() - start
-    print("Request completed in %f ms" % request_time)
+    try:
+        start = time.clock()
+        r = requests.get("https://iin.omise.co/v1/%s" % random.choice(iin_list), timeout=1)
+        request_time = time.clock() - start
+        print("Round %i request completed in %f ms" % (round, request_time))
+        round+=1
+    except Exception as e:
+        print(e)
+        break
+
